be7033407be0fb61ed93a7b1c729076f675f9d59
lidiane <lidianefernandesdiniz.lfd@gmail.com>
2023-04-04 22:14:50 -0300

    Formulário do cadastro

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,33 +1,52 @@
-import { Button, TextInput } from '@ignite-ui/react'
-import { Form } from './styles'
+import { Button, TextInput, Text } from '@ignite-ui/react'
+import { Form, FormAnnotation } from './styles'
 import { ArrowRight } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 const claimUserNameFormSchema = z.object({
-  username: z.string(),
+  username: z
+    .string()
+    .min(3, { message: 'O nome de usuário deve conter no mínimo 3 letras' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'O usuário deve ter apenas letras e hifens',
+    })
+    .transform((username) => username.toLowerCase()),
 })
 
 type ClaimUserNameFormData = z.infer<typeof claimUserNameFormSchema>
 
 export function ClaimUsernameForm() {
-  const { register, handleSubmit } = useForm<ClaimUserNameFormData>()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClaimUserNameFormData>({
+    resolver: zodResolver(claimUserNameFormSchema),
+  })
 
   async function handleClaimUsername(data: ClaimUserNameFormData) {
     console.log(data)
   }
   return (
-    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
-      <TextInput
-        size="sm"
-        prefix="ignite.com/"
-        placeholder="seu-usuario"
-        {...register('username')}
-      />
-      <Button size="sm" type="submit">
-        Reservar
-        <ArrowRight />
-      </Button>
-    </Form>
+    <>
+      <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+        <TextInput
+          size="sm"
+          prefix="ignite.com/"
+          placeholder="seu-usuario"
+          {...register('username')}
+        />
+        <Button size="sm" type="submit">
+          Reservar
+          <ArrowRight />
+        </Button>
+      </Form>
+
+      <FormAnnotation>
+        <Text size="sm">{errors.username?.message}</Text>
+      </FormAnnotation>
+    </>
   )
 }
